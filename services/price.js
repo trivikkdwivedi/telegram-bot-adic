b6e5eb1a0afacd4f3a151433868a873034a718b3
trivikkdwivedi <trivikkdwivedi@gmail.com>
2025-11-17 13:39:26 +0300

    Fix headers in fetch requests for price functions

--- services/price.js
+++ services/price.js
@@ -1,14 +1,13 @@
 import fetch from "node-fetch";
 
-// BirdEye API Base
 const API = "https://public-api.birdeye.so";
 
-// Only SOL or SPL tokens allowed
+// Check if valid mint address
 export function isMint(str) {
   return /^[1-9A-HJ-NP-Za-km-z]{32,44}$/.test(str);
 }
 
-// --- Fetch SOL price ---
+// Fetch SOL price
 export async function getSolPrice() {
   const url = `${API}/defi/price?chain=solana&address=So11111111111111111111111111111111111111112`;
 
@@ -17,14 +16,15 @@ export async function getSolPrice() {
     headers: {
       accept: "application/json",
       "x-chain": "solana",
-    },
+      "x-referrer": "birdeye_bot"
+    }
   });
 
   const data = await res.json();
   return data.data?.value || null;
 }
 
-// --- Fetch SPL Token price ---
+// Fetch SPL token price
 export async function getTokenPrice(mint) {
   const url = `${API}/defi/price?chain=solana&address=${mint}`;
 
@@ -33,7 +33,8 @@ export async function getTokenPrice(mint) {
     headers: {
       accept: "application/json",
       "x-chain": "solana",
-    },
+      "x-referrer": "birdeye_bot"
+    }
   });
 
   const data = await res.json();
