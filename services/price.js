d86a5d373da70332bb69ad206e2a7bcacfd043e4
trivikkdwivedi <trivikkdwivedi@gmail.com>
2025-11-17 13:42:49 +0300

    Update fetch headers and improve price fetching

--- services/price.js
+++ services/price.js
@@ -2,41 +2,41 @@ import fetch from "node-fetch";
 
 const API = "https://public-api.birdeye.so";
 
-// Check if valid mint address
+// Validate mint address
 export function isMint(str) {
   return /^[1-9A-HJ-NP-Za-km-z]{32,44}$/.test(str);
 }
 
 // Fetch SOL price
 export async function getSolPrice() {
-  const url = `${API}/defi/price?chain=solana&address=So11111111111111111111111111111111111111112`;
+  const url = ${API}/defi/price?chain=solana&address=So11111111111111111111111111111111111111112;
 
   const res = await fetch(url, {
     method: "GET",
     headers: {
       accept: "application/json",
       "x-chain": "solana",
-      "x-referrer": "birdeye_bot"
+      "x-referrer": "celesto_bot"
     }
   });
 
-  const data = await res.json();
-  return data.data?.value || null;
+  const json = await res.json();
+  return json?.data?.value || null;
 }
 
-// Fetch SPL token price
+// Fetch SPL token price by mint
 export async function getTokenPrice(mint) {
-  const url = `${API}/defi/price?chain=solana&address=${mint}`;
+  const url = ${API}/defi/price?chain=solana&address=${mint};
 
   const res = await fetch(url, {
     method: "GET",
     headers: {
       accept: "application/json",
       "x-chain": "solana",
-      "x-referrer": "birdeye_bot"
+      "x-referrer": "celesto_bot"
     }
   });
 
-  const data = await res.json();
-  return data.data?.value || null;
+  const json = await res.json();
+  return json?.data?.value || null;
 }
